4790a2e12b9fabf9df623e126426874de884a455
shbinn <shbinn23@gmail.com>
2023-09-15 19:22:42 +0900

    페이징 처리 시작

--- src/test/java/com/board/BoardDaoTest.java
+++ src/test/java/com/board/BoardDaoTest.java
@@ -4,6 +4,9 @@ import com.board.dao.BoardDAO;
 import com.board.model.BoardVO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LoggerFactoryBinder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,6 +14,8 @@ import java.util.List;
 
 @SpringBootTest
 public class BoardDaoTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(BoardDaoTest.class);
 
     @Autowired
     private BoardDAO boardDAO;
@@ -57,10 +62,26 @@ public class BoardDaoTest {
     public void testInsertBoard() throws Exception {
         BoardVO boardVO = new BoardVO();
         boardVO.setCate_cd("1");
-        boardVO.setTitle("첫번째 게시물 입니다.");
-        boardVO.setContent("첫번째 게시물입니다.");
+//        boardVO.setTitle("첫번째 게시물 입니다.");
+//        boardVO.setContent("첫번째 게시물입니다.");
         boardVO.setTag("1");
         boardVO.setReg_id("1");
+
+        for (int i = 0; i < 1234; i++) {
+            boardVO.setTitle(i + " 번째 게시물입니다.");
+            boardVO.setContent(i + " 번째 게시물입니다.");
+
+            int result = boardDAO.insertBoard(boardVO);
+
+            logger.info("\n Insert Board Result " + result);
+
+            if (result == 1) {
+                logger.info("\n게시물 등록 성공");
+            } else {
+                logger.info("\n게시물 등록 실패");
+            }
+        }
+
         int result = boardDAO.insertBoard(boardVO);
 
         System.out.println("\n Insert Board Result " + result);
